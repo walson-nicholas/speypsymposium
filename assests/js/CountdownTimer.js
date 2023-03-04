d9f671beb398ca7ff7e783bbdb101b97805ab4d7
NICHOLAS\asimi <asimieanicholas@gmail.com>
2023-03-04 16:21:32 +0100

    first commit

---
+++ assests/js/CountdownTimer.js
@@ -0,0 +1,30 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Apr 14, 2023 10:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="cd..."
+  document.getElementById("cd1").innerHTML = days + "DAYS";
+  document.getElementById("cd2").innerHTML = hours + "HOURS";
+  document.getElementById("cd3").innerHTML = minutes + "MINS";
+  document.getElementById("cd4").innerHTML = seconds + "SEC";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
